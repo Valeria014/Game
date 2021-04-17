fbb8a6cd0a40414f985ff89c6a285da4469014f9
Valeria014 <lera.dolls@mail.ru>
2021-04-17 20:00:22 +0900

    valeria014
    1

---
+++ ChessBoard.py
@@ -0,0 +1,212 @@
+from pieces.Bishop import Bishop
+from pieces.King import King
+from pieces.Knight import Knight
+from pieces.Pawn import Pawn
+from pieces.Queen import Queen
+
+from pieces.Rook import Rook
+
+
+class ChessBoard:
+    def __init__(self):
+        self.board = [[None for _ in range(8)] for _ in range(8)]
+        for i in range(8):
+            self.board[i][1] = Pawn('w', (i, 1))
+            self.board[i][6] = Pawn('b', (i, 6))
+
+        self.board[0][7] = Rook('b', (0, 7))
+        self.board[7][7] = Rook('b', (7, 7))
+        self.board[1][7] = Knight('b', (1, 7))
+        self.board[6][7] = Knight('b', (6, 7))
+        self.board[2][7] = Bishop('b', (2, 7))
+        self.board[5][7] = Bishop('b', (5, 7))
+        self.board[4][7] = King('b', (4, 7))
+        self.board[3][7] = Queen('b', (3, 7))
+
+        self.board[0][0] = Rook('w', (0, 0))
+        self.board[7][0] = Rook('w', (7, 0))
+        self.board[1][0] = Knight('w', (1, 0))
+        self.board[6][0] = Knight('w', (6, 0))
+        self.board[2][0] = Bishop('w', (2, 0))
+        self.board[5][0] = Bishop('w', (5, 0))
+        self.board[4][0] = King('w', (4, 0))
+        self.board[3][0] = Queen('w', (3, 0))
+
+        self.curr_player = 'w'
+
+        self.played_moves = []
+
+    def get_all_pieces(self):
+        """Возвращает список всех фигур на доске"""
+        pieces = []
+        for x in range(8):
+            for y in range(8):
+                if self.board[x][y]:
+                    pieces.append(self.board[x][y])
+        return pieces
+
+    def get_curr_player_pieces(self):
+        """Возвращаем все оставшиеся части текущего игрока"""
+        pieces = []
+        for piece in self.get_all_pieces():
+            if piece.color == self.curr_player:
+                pieces.append(piece)
+        return pieces
+
+    def get_poss_moves_for(self, piece):
+        """Возвращаем все возможные ходы для данной фигуры"""
+        # Необходимо проверить ход замка, если выбран король
+        if piece.name != 'King':
+            return piece.get_possible_moves(self.board)
+        else:
+            return piece.get_possible_moves(self.board) + self.get_castle_moves_for_curr_player()
+
+    def get_piece_at(self, space):
+        """Возращаем на место"""
+        return self.board[space[0]][space[1]]
+
+    def get_type_pieces_of_player(self, piece_name, player):
+        """Возвращаем все фигуры данного типа данного игрока"""
+        pieces = []
+        for row in self.board:
+            for space in row:
+                if space and space.name == piece_name and space.color == player:
+                    pieces.append(space)
+        return pieces
+
+    def move_piece(self, piece, new_position):
+        """Перемещение частей в новое положение и изменения has_moved значение true"""
+        pos = piece.position
+        piece_to_take = self.board[new_position[0]][new_position[1]]
+        piece.move(new_position)
+        self.board[new_position[0]][new_position[1]] = piece
+        self.board[pos[0]][pos[1]] = None
+        if not piece.has_moved:
+            piece.has_moved = True
+
+        if piece_to_take:
+            return piece_to_take
+        return None
+
+    def non_permanent_move(self, piece, new_position):
+        pos = piece.position
+        piece.move(new_position)
+        self.board[new_position[0]][new_position[1]] = piece
+        self.board[pos[0]][pos[1]] = None
+
+    def get_castle_moves_for_curr_player(self):
+        castles = []
+        king = self.get_type_pieces_of_player('King', self.curr_player)[0]
+        y = 0 if self.curr_player == 'w' else 7
+        b = self.board
+        # Проверка король и ладья не сдвинулись с места
+        if b[0][y] and not b[0][y].has_moved and b[4][y] and not b[4][y].has_moved:
+            # Check no pieces in between rook and king
+            if all(not b[x][y] for x in range(1, 4)):
+                if not self.king_in_check(king, self.board):
+                    castles.append((2, y))
+
+        if b[7][y] and not b[7][y].has_moved and b[4][y] and not b[4][y].has_moved:
+            if all(not b[x][y] for x in range(5, 7)):
+                if not self.king_in_check(king, self.board):
+                    castles.append((6, y))
+
+        # Имитируйте оба хода и проверьте игрок находится в чеке
+        for move in castles:
+            self.non_permanent_castle_king(king, move)
+            if self.king_in_check(king, self.board):
+                castles.remove(move)
+            self.uncastle_king(king)
+
+        return castles
+
+    def castle_king(self, king, new_king_position):
+        """дана королевская фигура и одна из четырех королевских позиций, которые могут составить замок. Перемещает короля в эту позицию и
+перемещает соответствующую ладью"""
+        # новые позиции короля = (2, 0), (6, 0), (2, 7), (6, 7)
+        corresponding_rook = {(2, 0): (0, 0), (6, 0): (7, 0), (2, 7): (0, 7), (6, 7): (7, 7)}
+        corresponding_rook_move = {(2, 0): (3, 0), (6, 0): (5, 0), (2, 7): (3, 7), (6, 7): (5, 7)}
+        rook_pos = corresponding_rook[new_king_position]
+        rook = self.board[rook_pos[0]][rook_pos[1]]
+        self.move_piece(king, new_king_position)
+        self.move_piece(rook, corresponding_rook_move[new_king_position])
+
+    def non_permanent_castle_king(self, king, new_king_position):
+        # NEW_KING_POS = (2, 0), (6, 0), (2, 7), (6, 7)
+        corresponding_rook = {(2, 0): (0, 0), (6, 0): (7, 0), (2, 7): (0, 7), (6, 7): (7, 7)}
+        corresponding_rook_move = {(2, 0): (3, 0), (6, 0): (5, 0), (2, 7): (3, 7), (6, 7): (5, 7)}
+        rook_pos = corresponding_rook[new_king_position]
+        rook = self.board[rook_pos[0]][rook_pos[1]]
+        self.non_permanent_move(king, new_king_position)
+        self.non_permanent_move(rook, corresponding_rook_move[new_king_position])
+
+    def uncastle_king(self, king):
+        """Отменяет ход замка"""
+        corresponding_rook = {(2, 0): (3, 0), (6, 0): (5, 0), (2, 7): (3, 7), (6, 7): (5, 7)}
+        corresponding_rook_move = {(2, 0): (0, 0), (6, 0): (7, 0), (2, 7): (0, 7), (6, 7): (7, 7)}
+        rook_pos = corresponding_rook[king.position]
+        rook = self.board[rook_pos[0]][rook_pos[1]]
+        self.non_permanent_move(rook, corresponding_rook_move[king.position])
+        self.non_permanent_move(king, (4, king.position[1]))
+
+
+    def king_in_check(self, king, b):
+        """Возвращается, если король в данный момент находится в чеке данной доски"""
+        # Проверяем наличие слона или ферзя на диагональном пути короля
+        for m in king.get_possible_diagonal_moves(b):
+            if b[m[0]][m[1]] and b[m[0]][m[1]].name in ['Bishop', 'Queen']:
+                return True
+
+        # проверка радьи или ферзя по прямой
+        for m in king.get_possible_straight_line_moves(b):
+            if b[m[0]][m[1]] and b[m[0]][m[1]].name in ['Rook', 'Queen']:
+                return True
+
+        # проверка наличие короля противника рядом с текущим королем
+        for m in king.get_possible_moves(b):
+            if b[m[0]][m[1]] and b[m[0]][m[1]].name == 'King':
+                return True
+
+        # Проверка  короля противника
+        for m in [(1, 2), (2, 1), (-1, 2), (-2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]:
+            pos = king.position[0] + m[0], king.position[1] + m[1]
+            if pos[0] < 0 or pos[0] > 7 or pos[1] < 0 or pos[1] > 7:
+                continue
+            if b[pos[0]][pos[1]] and b[pos[0]][pos[1]].name == 'Knight' \
+                    and b[pos[0]][pos[1]].color != self.curr_player:
+                return True
+
+        # проверяем наличие пешек противника
+        y_dir = 1 if self.curr_player == 'w' else -1
+        # Проверяем диагональ
+        l_diag = king.position[0] - 1, king.position[1] + y_dir
+        r_diag = king.position[0] + 1, king.position[1] + y_dir
+
+        if l_diag[0] >= 0:
+            if b[l_diag[0]][l_diag[1]] and b[l_diag[0]][l_diag[1]].name == 'Pawn' and b[l_diag[0]][l_diag[1]].color != self.curr_player:
+                return True
+        if r_diag[0] <= 7:
+            if b[r_diag[0]][r_diag[1]] and b[r_diag[0]][r_diag[1]].name == 'Pawn' and b[r_diag[0]][r_diag[1]].color != self.curr_player:
+                return True
+
+    def is_curr_player_in_check(self, piece, moves):
+        #Возвращает список допустимых ходов, которые не ставят игрока под контроль
+        king = self.get_type_pieces_of_player('King', self.curr_player)[0]
+        b = self.board
+        piece_original_pos = piece.position
+
+        poss_moves = []
+
+        for move in moves:
+            # Перемещаем фигуру на новый ход
+            piece_at_move_pos = self.board[move[0]][move[1]]
+            self.non_permanent_move(piece, move)
+
+            if not self.king_in_check(king, b):
+                poss_moves.append(move)
+
+            # Перемещаем фигуру обратно в исходное положение
+            self.non_permanent_move(piece, piece_original_pos)
+            self.board[move[0]][move[1]] = piece_at_move_pos
+
+        return poss_moves
---
+++ Game.py
@@ -0,0 +1,283 @@
+import pygame
+from pygame.locals import *
+import time
+from pieces.Queen import Queen
+
+from ChessBoard import ChessBoard
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.game_display = pygame.display.set_mode((900, 650))
+        pygame.display.set_caption('Chess')
+
+        self.settings = {'board_image': 'data/images/orange_board.png'}
+        self.board_image = pygame.image.load(self.settings['board_image'])
+
+        # Soundtrack
+        pygame.mixer.music.load('data/myz/1.mp3')
+        pygame.mixer.music.set_volume(0.1)
+        pygame.mixer.music.play(-1)
+
+        self.clock = pygame.time.Clock()
+        self.chess_board = ChessBoard()
+
+        self.curr_selected_piece = None
+        self.curr_poss_moves = []
+        self.all_poss_moves = self.get_all_poss_moves()
+
+        self.white_pieces_taken_images = []
+        self.black_pieces_taken_images = []
+
+        self.play_game()
+
+    def play_game(self):
+        """Цикл, который выполняет игру"""
+        while True:
+            # Рисуем все окна и доску
+            self.draw_window()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    # Получаем щелчок пользователя
+                    self.get_user_click()
+
+            # pygame.display.flip()
+            self.clock.tick(60)
+
+    def draw_window(self):
+        """Рисует все в окне"""
+        self.game_display.fill(white)
+        # рисуем боковое меню
+        self.draw_side_menu()
+        # рисуем нижнее меню
+        #рисуем доску
+        self.draw_board()
+        pygame.display.update()
+
+    def draw_side_menu(self):
+        """правое боковое меню"""
+        pygame.draw.rect(self.game_display, black, Rect((650, 100), (200, 400)), 5)
+
+        # рисуем поле вокруг текущего игрока
+        if self.chess_board.curr_player == 'b':
+            pygame.draw.rect(self.game_display, blue, Rect((700, 25), (100, 50)), 5)
+        else:
+            pygame.draw.rect(self.game_display, blue, Rect((700, 525), (100, 50)), 5)
+
+        self.message_display('Black', (750, 50), fontsize=30)
+        self.message_display('White', (750, 550), fontsize=30)
+
+        # показываем все ходы
+        start = 0
+        if len(self.get_all_played_moves()) > 10:
+            start = len(self.get_all_played_moves()) - 10
+        for i, move in enumerate(self.get_all_played_moves()[start:]):
+            self.message_display(move, (740, 125 + (i * 30)), fontsize=20)
+            start += 1
+
+        # показываем все взятые черные фигуры
+        for i, image in enumerate(self.black_pieces_taken_images):
+            image = pygame.image.load(image)
+            image = pygame.transform.scale(image, (30, 30))
+            pos = 610 + i * 20, 5
+            self.game_display.blit(image, pos)
+
+        # показываем все взятые белые фигуры
+        for i, image in enumerate(self.white_pieces_taken_images):
+            image = pygame.image.load(image)
+            image = pygame.transform.scale(image, (30, 30))
+            pos = 610 + i * 20, 595
+            self.game_display.blit(image, pos)
+
+    def draw_board(self):
+        """рисуем шахматную доску и все фигуры на доске"""
+        # рисуем шахматную доску
+        self.game_display.blit(self.board_image, (0, 0))
+
+        # рисуем фигуры на доске
+        for piece in self.chess_board.get_all_pieces():
+            image_position = piece.position
+            image_position = image_position[0] * 75, (7 - image_position[1]) * 75
+            piece_image = pygame.image.load(piece.image)
+            self.game_display.blit(piece_image, image_position)
+
+        # Проверяем выбрана ли часть в данный момент
+        # If yes:
+        if self.curr_selected_piece:
+            # выделяем эту часть
+            box_x, box_y = self.convert_space_to_coordinates(self.curr_selected_piece.position)
+            pygame.draw.rect(self.game_display, blue, Rect((box_x, box_y), (75, 75)), 5)
+            # отображаем все возможные ходы этой фигуры
+            for move in self.curr_poss_moves:
+                box1_x, box1_y = self.convert_space_to_coordinates(move)
+                pygame.draw.rect(self.game_display, red, Rect((box1_x, box1_y), (75, 75)), 5)
+
+    def get_user_click(self):
+        """анализируем позицию, на которую нажал пользователь."""
+        x, y = pygame.mouse.get_pos()
+        # определяем является ли щелчок:
+        # В нижнем меню
+        if y > 600:
+            pass
+        # В правом боковом меню
+        elif x > 600:
+            pass
+        # Если на доске:
+        else:
+            # преобразуем координаты в пространство
+            selected_space = self.convert_coordinates_to_space(x, y)
+            # если часть ещё не выбрана
+            if not self.curr_selected_piece:
+
+                # Vпроверяем и устанавливаем curr_selected_piece на эту часть
+                if self.is_piece_of_curr_player(selected_space):
+                    self.new_piece_selected(selected_space)
+
+            # если эта часть ещё выбрана
+            else:
+                # определяем находится ли выбранное пространство в возможных ходах
+
+                # Если пустота является текущим выбранным пространством
+                if selected_space == self.curr_selected_piece.position:
+                    self.deselect_piece()
+
+                # Иначе если пространство в возможных ходах:
+                elif selected_space in self.curr_poss_moves:
+                    #### проверяем, является ли фигура королем###
+                    # проверяем является ли выбранное пространство королем и в poss_castle_move
+                    if self.curr_selected_piece.name == 'King' and selected_space in self.chess_board.get_castle_moves_for_curr_player():
+                            # Замок этого короля
+                            self.add_move(self.curr_selected_piece.position, selected_space)
+                            self.chess_board.castle_king(self.curr_selected_piece, selected_space)
+
+                    else:
+                        # перемещаем выбраную фигура на это место
+                        self.add_move(self.curr_selected_piece.position, selected_space)
+                        self.move_piece(self.curr_selected_piece, selected_space)
+
+                        if self.curr_selected_piece.name == 'Pawn' and selected_space[1] == 0 or selected_space[1] == 7:
+                            self.chess_board.board[selected_space[0]][selected_space[1]] = None
+                            self.chess_board.board[selected_space[0]][selected_space[1]] = Queen(self.chess_board.curr_player, selected_space)
+
+                    # Отмените выбор текущей фигуры и удалить ходы
+                    self.deselect_piece()
+                    # Изменение текущего игрока
+                    self.change_curr_player()
+
+                    # проверяем наличие мата и получите новый список всех возможных ходов
+                    self.all_poss_moves = self.get_all_poss_moves()
+                    checkmate = True
+                    for piece_pos in self.all_poss_moves:
+                        if len(self.all_poss_moves[piece_pos]) != 0:
+                            checkmate = False
+                    if checkmate:
+                        self.draw_window()
+                        self.message_display('Checkmate!', (400, 300))
+                        winner = 'White' if self.chess_board.curr_player == 'b' else 'Black'
+                        self.message_display('%s wins!' % winner, (400, 400))
+                        pygame.display.update()
+                        time.sleep(2)
+                        quit()
+
+                # Иначе если еще один часть игрока:
+                elif selected_space in [piece.position for piece in self.chess_board.get_curr_player_pieces()]:
+                    # делаем эту часть текущей выбранной частью
+                    self.new_piece_selected(selected_space)
+
+                # Остальное (случайный, выбор места):
+                else:
+                    # Отменить текущий ход
+                    self.deselect_piece()
+
+    def convert_coordinates_to_space(self, x, y):
+        """Преобразует координаты (x, y) в соответствующее пространство на доске"""
+        # Доска нарисована вверх ногами, поэтому ось y перевернута
+        return x // 75, 7 - y // 75
+
+    def convert_space_to_coordinates(self, position):
+        """Возвращает координату верхнего левого угла, соответствующую заданному шахматному пятну"""
+        return position[0] * 75, (7 - position[1]) * 75
+
+    def is_piece_of_curr_player(self, space):
+        """Возвращает, если пробел содержит часть текущего игрока"""
+        for piece in self.chess_board.get_curr_player_pieces():
+            if space == piece.position:
+                return True
+
+    def get_all_poss_moves(self):
+        """Возвращает словарь всех возможных доступных ходов. ПРИМЕЧАНИЕ: вернет пустой список, если шах и мат"""
+        # словарь  позиции фигуры на возможные ходы
+        moves = {}
+        pieces = self.chess_board.get_curr_player_pieces()
+        for piece in pieces:
+            p_moves = self.chess_board.get_poss_moves_for(piece)
+            moves[piece.position] = self.chess_board.is_curr_player_in_check(piece, p_moves)
+        return moves
+
+    def get_curr_poss_moves(self):
+        """Возвращает возможные ходы, соответствующие выбранной в данный момент фигуре"""
+        return self.all_poss_moves[self.curr_selected_piece.position]
+
+    def get_all_played_moves(self):
+        return self.chess_board.played_moves
+
+    def move_piece(self, piece, new_position):
+        """Перемещает фигуру в новое положение и обновляет взятые фигуры"""
+        # Это  перемещает фигуру, не проверяет, является ли ход действительным
+        # Проверяет, взята ли фигура
+        piece_captured = self.chess_board.move_piece(piece, new_position)
+        if piece_captured:
+            self.piece_was_captured(piece_captured)
+
+    def change_curr_player(self):
+        """Изменение текущего игрока между "w" и "b"""
+        self.chess_board.curr_player = 'w' if self.chess_board.curr_player == 'b' else 'b'
+
+    def new_piece_selected(self, new_space):
+        """Устанавливаем новое пространство в curr_selected_piece и получает новые ходы для этой фигуры"""
+        self.curr_selected_piece = self.chess_board.get_piece_at(new_space)
+        self.curr_poss_moves = self.get_curr_poss_moves()
+
+    def deselect_piece(self):
+        """Отменяет выделение текущей части"""
+        self.curr_selected_piece = None
+        self.curr_poss_moves = None
+
+    def add_move(self, pos_1, pos_2):
+        """Добавить ход в список сыгранных ходов"""
+        name = self.chess_board.curr_player.upper() + ':     '
+        move = name + self.convert_coordinate_to_space_name(pos_1) + ' -> ' + self.convert_coordinate_to_space_name(pos_2)
+        self.chess_board.played_moves.append(move)
+
+    def convert_coordinate_to_space_name(self, coordinate):
+        """Возвращает преобразованное имя позиции """
+        conversions = {0 : 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H'}
+        return str(conversions[coordinate[0]]) + str(coordinate[1] + 1)
+
+    def piece_was_captured(self, piece):
+        """Обновляет список фрагментов, взятых для отображения в боковом меню"""
+        if piece.color == 'w':
+            self.white_pieces_taken_images.append(piece.image)
+        else:
+            self.black_pieces_taken_images.append(piece.image)
+
+    def message_display(self, text, point, fontsize=90):
+        """Отображает сообщение в окне"""
+        large_text = pygame.font.Font('freesansbold.ttf', fontsize)
+        text_surface = large_text.render(text, True, black)
+        text_rect = text_surface.get_rect()
+        text_rect.center = (point)
+        self.game_display.blit(text_surface, text_rect)
+
+if __name__ == '__main__':
+
+    white = (255,255,255)
+    blue = (34, 0, 255)
+    red = (209, 9, 9)
+    black = (0, 0, 0)
+    Game()
